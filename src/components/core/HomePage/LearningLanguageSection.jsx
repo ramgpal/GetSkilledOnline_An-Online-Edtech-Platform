f371748cefa77a94582a58966d0ed7b451ad18e8
ramgpal <ramgpal912@gmail.com>
2024-04-13 00:25:52 +0530

    learningLangauageSection component added

--- src/components/core/HomePage/LearningLanguageSection.jsx
+++ src/components/core/HomePage/LearningLanguageSection.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import HighlightText from "./HighlightText";
+import knowYourProgress from "../../../assets/Images/Know_your_progress.png";
+import compareWithOthers from "../../../assets/Images/Compare_with_others.png";
+import planYourLessons from "../../../assets/Images/Plan_your_lessons.png";
+import CTAButton from "./CTAButton";
 
 const LearningLanguageSection = () => {
     return(
-        <div></div>
+        <div className="mt-[150px]">
+            <div className="flex flex-col gap-5 items-center">
+                <div className="text-4xl text-center font-semibold">
+                    Your swiss knofe for <HighlightText text={"learning any language"}/>
+                </div>
+
+                <div className="text-center text-black-600 mx-auto text-base font-medium w-[70%]">Using spin making learning multiple languages easy. with 20+ languages realistic voice-over, progress tracking, custom schedule and more.</div>
+                
+                <div className="flex flex-row items-center justify-center mt-5">
+                    <img src = {knowYourProgress} className="object-contain -mr-32"/>
+                    <img src = {compareWithOthers} className="object-contain"/>
+                    <img src = {planYourLessons} className="object-contain -ml-36"/>
+                </div>
+
+                <div className="w-fit">
+                    <CTAButton active={true} linkto={"/signup"}>
+                        <div>Learn more</div>
+                    </CTAButton>
+                </div>
+            </div>
+        </div>
     )
 }
 
